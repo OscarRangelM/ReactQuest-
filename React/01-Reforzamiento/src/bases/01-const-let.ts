0d8bf870af898f4550d2f0fb0379b9c18db3b264
OscarRangelM <oscarmauricior7@gmail.com>
2026-04-13 23:16:52 -0600

    feat creamos un archivo para guardar el ejemplo de una asingación de datos sencilla con ts. Creamos el archivo vacio para un template de ejemplo

---
+++ React/01-Reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,4 @@
+let fisrtName: string = 'Oscar'; // de esta manera la indicamos a typescript que tipo de dato es la variable, en este caso un string. 
+const lasName = 'Rangel';
+
+console.log(fisrtName, lasName);
