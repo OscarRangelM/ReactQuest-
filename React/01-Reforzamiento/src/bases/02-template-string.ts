f9d8fa9eccf0a9038468ce90f5dcd83b03510136
OscarRangelM <oscarmauricior7@gmail.com>
2026-04-14 21:49:49 -0600

    feat: template string

---
+++ React/01-Reforzamiento/src/bases/02-template-string.ts
@@ -0,0 +1,5 @@
+const fisrtName: string = 'Oscar';
+const lasName: string = 'Rangel';
+const parrafoCompuesto: string = `Hola, mi nombre es ${fisrtName} ${lasName}, y estoy aprendiendo TypeScript`; // Esto es nuestro día a día, el template string, que nos permite escribir código de una manera más legible y fácil de entender, además de que nos permite insertar variables dentro de un string sin necesidad de concatenar.
+
+console.log(parrafoCompuesto);
